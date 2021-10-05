0233f65620ccdfd7e052921174f69f0b5528faa1
Jin Hao <limjinhao@gmail.com>
2021-10-05 15:50:06 +0800

    Add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('NUSCats')
+    .setDescription("API endpoint for NUSCats's backend")
+    .setVersion('1.0')
+    .build();
+
+  const swaggerDoc = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, swaggerDoc);
+
+  await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
