0352ec3959e5298684b844d28c3054bbda56c15d
acyang97 <acyang97@gmail.com>
2021-10-06 14:24:47 +0800

    Removed test stuff

--- src/meetings/meetings.controller.ts
+++ src/meetings/meetings.controller.ts
@@ -1,15 +1,9 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
-import { CreateMeetingDto } from './dto/create-meeting-dto';
 import { MeetingsService } from './meetings.service';
 
 @ApiTags('Meeting')
 @Controller('meeting')
 export class MeetingsController {
   constructor(private readonly meetingsService: MeetingsService) {}
-
-  @Post('/test')
-  public async createOne(@Body() createMeetingDto: CreateMeetingDto) {
-    return this.meetingsService.createOne(createMeetingDto);
-  }
 }
--- src/meetings/meetings.service.ts
+++ src/meetings/meetings.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { CreateMeetingDto } from './dto/create-meeting-dto';
 import { Meeting } from './meeting.entity';
 
 @Injectable()
@@ -10,11 +9,4 @@ export class MeetingsService {
     @InjectRepository(Meeting)
     private meetingRepository: Repository<Meeting>,
   ) {}
-
-  public async createOne(createMeetingDto: CreateMeetingDto): Promise<Meeting> {
-    const meeting = this.meetingRepository.create({
-      ...createMeetingDto,
-    });
-    return this.meetingRepository.save(meeting);
-  }
 }
