3437f7fa9e4d86f4abaca964119a378df074f1ba
Jin Hao <limjinhao@gmail.com>
2021-10-14 14:47:08 +0800

    Add deauthorize endpoint

--- src/zoom/zoom.service.ts
+++ src/zoom/zoom.service.ts
@@ -1,6 +1,11 @@
 import { catchError, map, Observable } from 'rxjs';
 import { HttpService } from '@nestjs/axios';
-import { ForbiddenException, HttpException, Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  HttpException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
@@ -11,17 +16,26 @@ import { ZoomMeetingListDto } from './dtos/zoom-meeting-list.dto';
 import { ZoomMeetingStatus } from '../shared/enum/zoom-meeting-status.enum';
 import { ZoomMeetingOptionsDto } from './dtos/zoom-meeting-options.dto';
 import { User } from '../users/user.entity';
+import {
+  ZoomDeauthorizePayload,
+  ZoomDeauthorizeSubscriptionDto,
+} from './dtos/zoom-subscriptions.dto';
+import { ZoomConfigService } from './../config/zoom.config';
+
 @Injectable()
 export class ZoomService {
   constructor(
-    private readonly httpService: HttpService,
     @InjectRepository(Meeting)
     private readonly meetingRepository: Repository<Meeting>,
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+    private readonly httpService: HttpService,
+    private readonly zoomConfig: ZoomConfigService,
   ) {}
 
   getUser(zoomToken: string): Observable<ZoomUser> {
     return this.httpService
-      .get(`users/me`, {
+      .get(`v2/users/me`, {
         headers: {
           Authorization: `Bearer ${zoomToken}`,
         },
@@ -36,7 +50,7 @@ export class ZoomService {
 
   getUpcomingMeetings(zoomToken: string): Observable<ZoomMeetingListDto> {
     return this.httpService
-      .get(`users/me/meetings`, {
+      .get(`v2/users/me/meetings`, {
         headers: {
           Authorization: `Bearer ${zoomToken}`,
         },
@@ -55,7 +69,7 @@ export class ZoomService {
 
   getMeeting(meetingId: number, zoomToken: string): Observable<ZoomMeetingDto> {
     return this.httpService
-      .get(`meetings/${meetingId}`, {
+      .get(`v2/meetings/${meetingId}`, {
         headers: {
           Authorization: `Bearer ${zoomToken}`,
         },
@@ -108,5 +122,61 @@ export class ZoomService {
       agendaItems: options.agendaItems || [],
     });
     return this.meetingRepository.save(meetingToCreate);
+  }
+
+  async deauthorizeUser(
+    verificationToken: string,
+    deauthorizeDetail: ZoomDeauthorizeSubscriptionDto,
+  ) {
+    const { payload, event } = deauthorizeDetail;
+
+    if (verificationToken !== this.zoomConfig.verificationToken) {
+      throw new UnauthorizedException('Invalid verification token');
+    }
+
+    if (event !== 'app_deauthorized') {
+      throw new UnauthorizedException('Invalid subscription type');
+    }
+
+    const user = await this.userRepository.findOne({ zoomId: payload.user_id });
+    if (user) {
+      if (user.isEmailConfirmed) {
+        // user has other account type
+        // unlink from zoom
+        user.zoomId = null;
+        await this.userRepository.save(user);
+        console.log('Unlinked zoom user');
+      } else {
+        // Delete account
+        await this.userRepository.remove(user);
+        console.log('Deleted zoom user');
+      }
+    } else {
+      console.log('Error: user not found in db');
+    }
+
+    return this.httpService
+      .post(
+        `oauth/data/compliance`,
+        {
+          client_id: this.zoomConfig.clientId,
+          user_id: payload.user_id,
+          account_id: payload.account_id,
+          deauthorization_event_received: payload,
+          compliance_completed: true,
+        },
+        {
+          auth: {
+            username: this.zoomConfig.clientId,
+            password: this.zoomConfig.clientSecret,
+          },
+        },
+      )
+      .pipe(
+        map((res) => res.data as ZoomDeauthorizePayload),
+        catchError((e) => {
+          throw new HttpException(e.response.data, e.response.status);
+        }),
+      );
   }
 }
