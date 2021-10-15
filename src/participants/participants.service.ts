214b987fe17a7379d89f0f6d4754916f0b73e4a4
Jin Hao <limjinhao@gmail.com>
2021-10-15 13:57:02 +0800

    Fix invitation update

--- src/participants/participants.service.ts
+++ src/participants/participants.service.ts
@@ -200,7 +200,7 @@ export class ParticipantsService {
     return this.participantsRepository.save({
       ...participant,
       hashedMagicLinkToken: hashedMagicLinkToken,
-      newInvited: true,
+      invited: true,
     });
   }
 
